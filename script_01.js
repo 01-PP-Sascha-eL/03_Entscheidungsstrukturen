599bc5b63cf1b56a4b50aa434d04f60259b867ee
ShatterAr <sascha.s.89@gmx.de>
2023-05-10 11:56:59 +0200

    ok

--- script_01.js
+++ script_01.js
@@ -72,14 +72,32 @@
 const firstName = "Jane";
 let job;
 
-job = "driver"; // .. fährt TAXI! / UBER
+job = "driver";     // .. fährt TAXI! / UBER
+job = "diver";      // .. taucht im Rhein!
+job = "artist";     // .. malt ein Bild!
+job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+
 
 switch (job) 
 {
     case "driver": // ( == )
         console.log (firstName + " fährt TAXI!");
+        break;     
+    case "diver": // ( == )
+        console.log (firstName + " taucht im Rhein!");
         break;
+    case "artist": // ( == )
+        console.log (firstName + " malt ein Bild!");
+        break;
+    case "pilot": // ( == )
+        console.log (firstName + " fliegt ein Flugzeig!");
+        break;
+    case "teacher": // ( == )
+        console.log (firstName + " unterrichtet!");
+        break; 
 
     default:
+        console.log(firstName + " macht etwas anderes!");
         break;
 }
